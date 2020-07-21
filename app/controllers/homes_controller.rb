5c9308244a520c3a0d57f802c9e67e1636a87f61
Billy Leung <billy.kfleung@gmail.com>
2020-07-21 22:32:17 +0800

    user can add a home with shares of ownership and set as admin

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,15 +2,17 @@ class HomesController < ApplicationController
 
   def index
   end
-  
+
   def new
     @home = Home.new
+    authorize @home
   end
 
   def create
     @home = Home.new(home_params)
-    @home.user = current_user
+    authorize @home
     if @home.save
+      create_ownership
       redirect_to homes_path, notice: 'Your home was successfully created!'
     else
       render :new
@@ -21,5 +23,14 @@ class HomesController < ApplicationController
 
   def home_params
     params.require(:home).permit(:country, :city, :address, :maximum_no_of_guest, :no_of_bed)
+  end
 
+  def create_ownership
+    @ownership = Ownership.new
+    @ownership.user = current_user
+    @ownership.is_admin = true
+    @ownership.home = @home
+    @ownership.shares_of_ownership = params["shares_of_ownership"]
+    @ownership.save
+  end
 end
--- app/policies/home_policy.rb
+++ app/policies/home_policy.rb
@@ -4,4 +4,8 @@ class HomePolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def create?
+    true
+  end
 end
