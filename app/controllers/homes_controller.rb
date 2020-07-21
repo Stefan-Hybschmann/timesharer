37feb2bc291621eec76702aee591bf85741ca38b
Billy Leung <billy.kfleung@gmail.com>
2020-07-21 19:42:54 +0800

    user add a home - v1

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,2 +1,21 @@
 class HomesController < ApplicationController
+  def new
+    @home = Home.new
+  end
+
+  def create
+    @home = Home.new(home_params)
+    @home.user = current_user
+    if @home.save
+      redirect_to homes_path, notice: 'Your home was successfully created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def home_params
+    params.require(:home).permit(:country, :city, :address, :maximum_no_of_guest, :no_of_bed)
+  end
 end
