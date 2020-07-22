9077b4689d9fd1143155c52afd669c7691676e85
Stefan Hybschmann <stefan.hybschmann@gmail.com>
2020-07-22 13:07:56 +0200

    Index of homes and bookings are now only showing the homes and bookings of the user who is signed in

--- app/policies/home_policy.rb
+++ app/policies/home_policy.rb
@@ -7,12 +7,12 @@ class HomePolicy < ApplicationPolicy
     def index?
       true
     end
-  end
-  def show?
-    true
-  end
+    def show?
+      true
+    end
 
-  def create?
-    true
+    def create?
+      true
+    end
   end
 end
