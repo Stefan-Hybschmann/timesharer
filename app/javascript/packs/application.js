126b7c57d04627f401e33f221feaec005bfe5494
Stefan Hybschmann <stefan.hybschmann@gmail.com>
2020-07-27 10:27:21 +0200

    typos because of copy paste from slack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,35 +2,29 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-​
-​
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-​
-​
-// ----------------------------------------------------
-// Note(lewagon): ABOVE IS RAILS DEFAULT CONFIGURATION
-// WRITE YOUR OWN JS STARTING FROM HERE 👇
-// ----------------------------------------------------
-​
 // External imports
+
 import "bootstrap";
-​
-​
+
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+
 import { initBookingsCalendar, adjustCalendarSize } from '../plugins/bookings-calendar';
 import {flatpicker} from "../plugins/flatpickr"
-​
+
+import { initMapbox } from '../plugins/init_mapbox';
+
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+
   flatpicker();
   initBookingsCalendar();
+  initMapbox();
 });
 
 document.addEventListener('readystatechange', (event) => {
@@ -39,8 +33,3 @@ document.addEventListener('readystatechange', (event) => {
   }
 });
 
-import { initMapbox } from '../plugins/init_mapbox';
-
-document.addEventListener('turbolinks:load', () => {
-  initMapbox();
-})
