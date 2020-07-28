2aee8e10d967d0ec68c4d5adda126982949ac122
Billy Leung <billy.kfleung@gmail.com>
2020-07-28 18:12:35 +0800

    added ranking for shares of ownership, including pending status and pending page

--- app/models/home.rb
+++ app/models/home.rb
@@ -10,17 +10,17 @@ class Home < ApplicationRecord
   validates :maximum_no_of_guest, :no_of_bed, numericality: { greater_than_or_equal_to: 0 }
 
   # For ranking by shares of ownerships, DO NOT DELETE
-  # def bigger_owners(current_user)
-  #   current_user_ownership = ownerships.find do |ownership|
-  #     ownership.user == current_user
-  #   end
+  def bigger_owners(current_user)
+    current_user_ownership = ownerships.find do |ownership|
+      ownership.user == current_user
+    end
 
-  #   bigger_ownerships = ownerships.select do |ownership|
-  #     ownership.shares_of_ownership > current_user_ownership.shares_of_ownership
-  #   end
+    bigger_ownerships = ownerships.select do |ownership|
+      ownership.shares_of_ownership > current_user_ownership.shares_of_ownership
+    end
 
-  #   bigger_ownerships.map(&:user)
-  # end
+    bigger_ownerships.map(&:user)
+  end
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_28_074755) do
+ActiveRecord::Schema.define(version: 2020_07_28_092236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2020_07_28_074755) do
     t.datetime "updated_at", precision: 6, null: false
     t.date "start_date"
     t.date "end_date"
+    t.string "status", default: "Confirmed"
     t.index ["home_id"], name: "index_bookings_on_home_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
